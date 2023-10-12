43fd92326d963515c5c3f0cc823313fe2ca1c978
Matt Brictson <matt@mattbrictson.com>
2023-10-12 16:55:23 -0700

    Update all gems (except rails) (#488)
    This commit updates all gems in preparation for Rails 7.1.
    
    ```
    irb                1.7.4  → 1.8.2
    loofah             2.21.3 → 2.21.4
    net-imap           0.3.7  → 0.4.1
    net-smtp           0.3.3  → 0.4.0
    psych              (new)  → 5.1.1
    rdoc               (new)  → 6.5.0
    redis-client       0.16.0 → 0.17.0
    regexp_parser      2.8.1  → 2.8.2
    reline             0.3.8  → 0.3.9
    selenium-webdriver 4.13.1 → 4.14.0
    sprockets          4.2.0  → 4.2.1
    sqlite3            1.6.6  → 1.6.7
    stimulus-rails     1.2.2  → 1.3.0
    stringio           (new)  → 3.0.8
    turbo-rails        1.4.0  → 1.5.0
    zeitwerk           2.6.11 → 2.6.12
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_154519) do
-
+ActiveRecord::Schema[7.0].define(version: 2019_12_26_154519) do
 end
