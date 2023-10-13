202d0edfe61ca5b9bf98cf5a738b3953dec212d2
Matt Brictson <matt@mattbrictson.com>
2023-10-12 17:01:34 -0700

    Upgrade to Rails 7.1.1 (#489)
    * Upgrade to Rails 7.1.1
    
    * Run app:update and db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2019_12_26_154519) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_12_235452) do
 end
